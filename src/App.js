bcadbc8829d92b24bb82fbdabbf62ff6915bf36f
nahomypolycar <112959201+nahomypolycar@users.noreply.github.com>
2023-01-21 15:03:39 +0100

    essai de visualisation des pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Offer from "./pages/Offer";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  return;
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/offer" element={<Offer />}></Route>
+    </Routes>
+  </Router>;
 }
 
 export default App;
---
+++ src/pages/Offer.js
@@ -0,0 +1,5 @@
+const Offer = () => {
+  return <div>Offre de produit</div>;
+};
+
+export default Offer;
