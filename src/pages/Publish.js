f9dd807af749905c7380b692a6a1f5651106901a
nahomypolycar <112959201+nahomypolycar@users.noreply.github.com>
2023-02-12 23:37:29 +0100

    add states on publish page

--- src/pages/Publish.js
+++ src/pages/Publish.js
@@ -2,6 +2,16 @@ import Cookies from "js-cookie";
 import { Navigate } from "react-router-dom";
 
 const Publish = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [condition, setCondition] = useState("");
+  const [city, setCity] = useState("");
+  const [brand, setBrand] = useState("");
+  const [size, setSize] = useState("");
+  const [color, setColor] = useState("");
+  const [picture, setPicture] = useState("");
+
   const token = Cookies.get("token");
   console.log(token);
   return token ? (
